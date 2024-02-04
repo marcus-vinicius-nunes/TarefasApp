a82fea28d332581ac73e7566181a5620e49c9932
marcus-vinicius-nunes <vinicius_rj87@yahoo.com.br>
2024-02-04 11:16:22 -0300

    fisrt commit

---
+++ TarefasApp.IntegrationTests/TarefasTest.cs
@@ -0,0 +1,32 @@
+namespace TarefasApp.IntegrationTests
+{
+    public class TarefasTest
+    {
+        /// <summary>
+        /// Teste de integracao de ENDPOINT da API de tarefas
+        /// </summary>
+        [Fact(Skip = "Não implentado.")]
+        public async Task PostTarefas_ReturnsSucessStatusCode()
+        {
+            await Task.CompletedTask;
+        }
+
+        [Fact(Skip = "Não implentado.")]
+        public async Task PutTarefas_ReturnsSucessStatusCode()
+        {
+            await Task.CompletedTask;
+        }
+
+        [Fact(Skip = "Não implentado.")]
+        public async Task DeletetTarefas_ReturnsSucessStatusCode()
+        {
+            await Task.CompletedTask;
+        }
+
+        [Fact(Skip = "Não implentado.")]
+        public async Task GetTarefas_ReturnsSucessStatusCode()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
